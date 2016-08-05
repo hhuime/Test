dac85e256c0c3668e912a3c25b5f4674d83083d1
huanghui <hui.huang@forufamily.com>
2016-08-05 10:34:44 +0800

    update widget

--- Application/Home/Widget/OrderWidget.class.php
+++ Application/Home/Widget/OrderWidget.class.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Home\Widget;
 use Think\Controller;
 
@@ -6,11 +7,16 @@ class OrderWidget extends Controller{
 
 	function order(){
     	Vendor('Hprose.HproseHttpClient');
+    	/*
     	$url = "http://openrpc.zuimeimami.com/mamimallweb/";
     	$uid = "b719083891941671578dc3652f6ea";
 		$client = new \HproseHttpClient($url . "order/");
 		$result = json_decode($client->appmalluserorderdata($uid, '', 1), true);
 		$this->assign('data', $result['data']);
+		*/
+		$client = new \HproseHttpClient("http://localhost/test/test/test.php");
+		$result = $client->Hello("Hprose");
+		$this->assign('data', $result);
 		$this->display('order/order');
 	}
 }
