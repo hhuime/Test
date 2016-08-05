a60f91eea33edc083db9e3f34d141785ef8fcc0a
huanghui <hui.huang@forufamily.com>
2016-08-05 18:03:14 +0800

    commit

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -37,14 +37,40 @@ class IndexController extends CommonController {
         //$data = $redis->redis();
         //$result = $data->get('test');
         //$result = $redis->editval('test', '2333'); //修改key
-        $val = array('a','b','c','d','e','f',1,2,3,4,5);
-        $result = $redis->addHashval('Hash', abc, $val);
+
+        //$val = array('a','b','c','d','e','f',1,2,3,4,5);
+        //$result = $redis->addHashval('Hash', abc, $val); //添加Hash数据
+
+        $result = $redis->getoneHashval('Hash', abc); //获取某个Hash数据
 /*
         $redis->init(eval(C('REDIS')));
         $redis->select(0);
         $result = json_decode($redis->getoneHashval('Order', 'D120682657845575'), true);
 */
-        dump($result);
+        var_dump($result);
+    }
+
+    public function cash(){
+    	S('a', 2333);
+    	//S('a', null);
+    	$cash = S('a');
+    	var_dump($cash);
+    }
+
+    public function cookie(){
+    	cookie('abc', 233);
+    	cookie('123', 'aaaaa');
+    	//$cookie = cookie('abc');
+    	$cookie = $_COOKIE;
+    	var_dump($cookie);
+    }
+
+    public function session(){
+    	session('233', 'abcd');
+    	session('abb', '1111');
+    	//$session = session('233');
+    	$session = $_SESSION;
+    	var_dump($session);
     }
 
 }
